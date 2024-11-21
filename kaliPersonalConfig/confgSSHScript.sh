fcf0c9036894d7d9dabbbde9b4fd1e8c7ea1714f
Unnamed10110 <trojan.v6@gmail.com>
2024-11-21 11:39:39 -0300

    autoUpdateScriptForKali

---
+++ kaliPersonalConfig/confgSSHScript.sh
@@ -0,0 +1,10 @@
+cd ~
+
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+sudo git clone https://github.com/Unnamed10110/zsh-pwsh-wt.git
+
+ cp -r zsh-pwsh-wt/kaliPersonalConfig/debian/Home/.  .
+
+ 
