73a53a18dc45450822e61098d5bfc61745fee666
Unnamed10110 <trojan.v6@gmail.com>
2024-11-24 14:59:51 -0300

    script update

--- checkLicense.ps1
+++
@@ -1,3 +0,0 @@
-pwsh -command "& {Get-WmiObject -Query 'SELECT * FROM SoftwareLicensingService' | Select-Object OA3xOriginalProductKey, LicenseStatus, Description, @{Name='WindowsVersion';Expression={(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').ProductName}}}"
-pause
-
---
+++ checkLicenseANDhw.ps1
@@ -0,0 +1,21 @@
+pwsh -command "& {Get-WmiObject -Query 'SELECT * FROM SoftwareLicensingService' | Select-Object OA3xOriginalProductKey, LicenseStatus, Description, @{Name='WindowsVersion';Expression={(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').ProductName}}}"
+
+
+    # Get the Windows activation and version info
+    $licenseInfo = Get-WmiObject -Query 'SELECT * FROM SoftwareLicensingService' | Select-Object OA3xOriginalProductKey, LicenseStatus, Description, @{Name='WindowsVersion';Expression={(Get-ItemProperty -Path ''HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'').ProductName}}
+
+    # Get hardware information
+    $computerInfo = Get-WmiObject -Class Win32_ComputerSystem | Select-Object Manufacturer, Model, Name, SystemType
+    $biosInfo = Get-WmiObject -Class Win32_BIOS | Select-Object SerialNumber
+
+    # Display the information
+    Write-Output 'Windows Activation and Version Info:'
+    $licenseInfo | Format-List
+
+    Write-Output 'Computer Hardware Info:'
+    $computerInfo | Format-List
+
+    Write-Output 'BIOS Info:'
+    $biosInfo | Format-List
+
+    pause
