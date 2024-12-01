1a735acd355ba17c98085c79f7359561813762b2
Unnamed10110 <trojan.v6@gmail.com>
2024-12-01 05:41:09 -0300

    uselss script FOR NOW

---
+++ scripts/repairReg.ps1
@@ -0,0 +1,92 @@
+Windows Registry Editor Version 5.00
+
+; Spanish Keyboard Layouts
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\0000040A]
+"Layout File"="KBDSP.DLL"
+"Layout Text"="Spanish"
+
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\0000080A]
+"Layout File"="KBDLA.DLL"
+"Layout Text"="Spanish (Latin America)"
+
+; English Keyboard Layouts
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000409]
+"Layout File"="KBDUS.DLL"
+"Layout Text"="US"
+
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000809]
+"Layout File"="KBDUK.DLL"
+"Layout Text"="United Kingdom"
+
+; French Keyboard Layouts
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\0000040C]
+"Layout File"="KBDFR.DLL"
+"Layout Text"="French"
+
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\0000080C]
+"Layout File"="KBDCF.DLL"
+"Layout Text"="French (Belgian)"
+
+; German Keyboard Layouts
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000407]
+"Layout File"="KBDGR.DLL"
+"Layout Text"="German"
+
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000807]
+"Layout File"="KBDSG.DLL"
+"Layout Text"="Swiss German"
+
+; Italian Keyboard Layouts
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000410]
+"Layout File"="KBDIT.DLL"
+"Layout Text"="Italian"
+
+; Japanese Keyboard Layout
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000411]
+"Layout File"="KBDJPN.DLL"
+"Layout Text"="Japanese"
+
+; Chinese (Simplified) Keyboard Layouts
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000804]
+"Layout File"="KBDUS.DLL"
+"Layout Text"="Chinese (Simplified) - US Keyboard"
+
+; Korean Keyboard Layout
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000412]
+"Layout File"="KBDKOR.DLL"
+"Layout Text"="Korean"
+
+; Russian Keyboard Layout
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000419]
+"Layout File"="KBDRU.DLL"
+"Layout Text"="Russian"
+
+; Arabic Keyboard Layout
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000401]
+"Layout File"="KBDAR.DLL"
+"Layout Text"="Arabic"
+
+; Hebrew Keyboard Layout
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\0000040D]
+"Layout File"="KBDHE.DLL"
+"Layout Text"="Hebrew"
+
+; Portuguese Keyboard Layouts
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000416]
+"Layout File"="KBDPO.DLL"
+"Layout Text"="Portuguese (Brazil)"
+
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000816]
+"Layout File"="KBDPT.DLL"
+"Layout Text"="Portuguese (Portugal)"
+
+; Additional Layouts (if needed, add here)
+; Polish
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000415]
+"Layout File"="KBDPL.DLL"
+"Layout Text"="Polish"
+
+; Dutch
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000413]
+"Layout File"="KBDNL.DLL"
+"Layout Text"="Dutch"
