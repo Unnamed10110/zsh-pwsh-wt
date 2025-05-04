7910b154ff849eab4424543db1e122ecdadb57a2
Unnamed10110 <28940464+Unnamed10110@users.noreply.github.com>
2025-05-04 08:01:08 -0300

    ap

--- kaliPersonalConfig/automate_zsh.sh
+++ kaliPersonalConfig/automate_zsh.sh
@@ -48,6 +48,3 @@ sudo ln -sf /home/YOUR_USERNAME/.zsh_history /root/.zsh_history
 chmod 755 /home/linuxbrew/.linuxbrew/share/zsh/site-functions/_brew
 chmod -R go-w /home/linuxbrew/.linuxbrew/share/zsh/site-functions/_brew
 
-# sudo, config to root
-
-chmod -R go-w /home/linuxbrew/.linuxbrew/share/zsh/site-functions/_brew && sed -i 's/\r\$//' ~/.zshrc && find ~/.oh-my-zsh -type f -exec sed -i 's/\r\$//' {} + && source ~/.zshrc && sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\" && cp ~/.zshrc /root/ && cp -rf ~/.oh-my-zsh /root/ && chown -R root:root /root/.zshrc /root/.oh-my-zsh && source ~/.zshrc && clear && sleep 4 && exit
